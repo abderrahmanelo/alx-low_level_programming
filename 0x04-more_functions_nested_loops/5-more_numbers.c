05f21df32882b43826c6877f63c7b2d9be39e6b3
abderrahmanot <abderrahmaot@gmail.com>
2023-07-20 05:54:40 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,15 @@
  */
 void more_numbers(void)
 {
-	int i, j, k;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
-		}
-		for (k = 10; k < 15; k++)
-		{
-			_putchar(k / 10 + '0');
-			_putchar(k % 10 + '0');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
