eeb2f1e9b4169119dcb461e207ee6b8dedd13a9e
abderrahmanot <abderrahmaot@gmail.com>
2023-07-24 04:41:06 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -18,18 +18,15 @@ void puts2(char *str)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
-/*	int len, i;
- *
- *	len = 0;
- *
- *	while (str[len] != '\0')
- *	{
- *		len++;
- *	}
- *
- *	for (i = 0; i < len; i += 2)
- *	{
- *		_putchar(str[i]);
- *	}
- */
+
+/*	int len, i;*/
+/*	len = 0;*/
+/*	while (str[len] != '\0')*/
+/*	{*/
+/*		len++;*/
+/*	}*/
+/*	for (i = 0; i < len; i += 2)*/
+/*	{*/
+/*		_putchar(str[i]);*/
+/*	}*/
 }
